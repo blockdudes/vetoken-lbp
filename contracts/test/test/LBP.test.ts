f9e5480e4100687b7d218255ccbb32a327a9c7fd
DhruvJain1122 <amritkumarjain321@gmail.com>
2022-09-14 05:22:22 +0000

    feat: test

---
+++ contracts/test/test/LBP.test.ts
@@ -0,0 +1,5 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { BigNumber } from 'ethers';
+import '@balancer-labs/v2-pool-weighted/contracts/smart/LiquidityBootstrappingPool';
+
